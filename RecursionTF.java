249b9fd9c5d0b03457f519b089068e74e2345605
juliej3141 <98037714+juliej3141@users.noreply.github.com>
2023-03-08 09:23:23 -0500

    Add files via upload

---
+++ RecursionTF.java
@@ -0,0 +1,14 @@
+import doodlepad.*;
+import com.phidget22.*;
+
+public class RecursionTF {
+	
+	public Text​(java.lang.String text, double x, double y) {
+		
+	}
+	public static void main(String[] args) {
+      Rectangle r1 = new Rectangle(60, 70, 400, 150);
+      Rectangle r3 = new Rectangle(260, 220, 200, 150);
+      Rectangle r4 = new Rectangle(60, 220, 200, 150);
+    }
+}
