226174755774434c9b874fceeaf5f395b5084aa9
Kikoriddle <98037761+kikorid@users.noreply.github.com>
2023-03-08 09:29:33 -0500

    Add files via upload
    testing files for the questions

---
+++ Testing.java
@@ -0,0 +1,31 @@
+public class Testing{
+// 
+}
+
+/*Question 3-tested 
+ * public static void main(String[] args) {
+System.out.println(recurs(10));
+} 
+public static int recurs(int count){
+	if (count == 0){
+		return 0;
+	} else {
+		System.out.print(count + " ");
+		int recount = count - 2;
+		return recurs(recount);
+	}
+}
+*/
+
+/*Question 4- tested 
+ * public static void main(String[] args) {
+		System.out.println(equation(8));
+	} 
+	public static int equation(int a) {
+        if(a <= 5) {
+        	return 12;
+        }else{
+        	return equation(a-2) * equation(a-1);
+		}
+	}
+*/
